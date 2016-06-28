b22cbc359b429e14ed0436e3a2f67632317f3f30
Acacia Mack <acacialmack@gmail.com>
2016-06-28 10:25:18 -0400

    only move right and left when field is showing

--- js/app.js
+++ js/app.js
@@ -24,15 +24,15 @@ window.addEventListener("keydown", function(e) {
 //if ball enters goal, player gets 1 point
 
 var player           = document.getElementById('player');
-var home             = document.getElementById('home');
-var game             = document.getElementById('game')
+var home             = document.getElementsByClassName('home');
+var field            = document.getElementById('game');
 var ball             = document.getElementById('ball');
 var goal             = document.getElementById('goal')
 var start            = 40; //43
 var currentPosition  = parseInt(getComputedStyle(player).left);
-var ballPosition     = parseInt(getComputedStyle(ball).left);
+// var ballPosition     = parseInt(getComputedStyle(ball).left);
 var end              = 220; //218
-var shots             = 0;
+var shots            = 0;
 
 
 // function newPosition(){
@@ -45,42 +45,68 @@ var shots             = 0;
 //     var nw = Math.floor(Math.random() * w);
     
 //     return [nh,nw];    
-    
+//     console.log(nw)
+// }
+
+// newPosition();
+
+function threeShots() {
+  shots++
+  if(shots < 3){
+    animateDiv()
+    }else{
+      console.log("game over")
+    }
+}
+
+//paddle collision
+// function paddleCollision(){
+//   var points = 0;
+//   var ballPositionX;
+//   var ballPositionY;
+
+//   if(player.style.left === ballPositionX || ballPositionY){
+//     points++
+//     console.log("point scored")
+//   }else if(player.style.left < ballPositionX || ballPositionY){
+//     points--
+//     console.log("point loss")
+//   }
 // }
 
 //ball moves toward goal 3 times 
 function animateDiv(){
-  shots++
-  if(shots < 3) {
+    var x = field.offsetWidth - 150;
+    var y = field.offsetHeight - 150;
+    console.log("x = " + x);
+    console.log("y = " + y);
 
-    var x = [0,100,200,50,25];
-    var y = [150,250,0,0.5,2];
 
-    var randX = x[Math.floor(Math.random() * x.length)];
-    var randY = y[Math.floor(Math.random() * y.length)];
+    var randX = Math.floor(Math.random(x) * x);
+    var randY = Math.floor(Math.random(y) * y);
+
+    console.log(randX)
+    console.log(randY)
 
     var ballPositionX = randX;
     var ballPositionY = randY;
     console.log(currentPosition);  
     var id = setInterval(frame, 10);
-   
+   console.log(id)
     function frame() {
       if (ballPositionX == 170 || ballPositionY == 240) {
         clearInterval(id);
       } else {
         ballPositionX++;
         ballPositionY++;
-        console.log("Y = " + ballPositionY);
-        console.log("X = " + ballPositionX);
+        // console.log("Y = " + ballPositionY);
+        // console.log("X = " + ballPositionX);
         ball.style.top =  ballPositionY + 'px';
         ball.style.left = ballPositionX + 'px';
       }
     }
   } 
 
-   
-    
-};
 
 // function calcSpeed(prev, next) {
     
@@ -103,6 +129,10 @@ if(currentPosition < end) {
   console.log("move right");
   console.log(currentPosition);
     player.style.left = currentPosition + 5 + 'px';
+    //collision
+  }else if(currentPosition === ballPositionX || ballPositionY){
+    points++
+    console.log("point scored")
   }
 }
 
@@ -111,6 +141,10 @@ if(currentPosition > start) {
   console.log("move left");
   console.log(currentPosition);
     player.style.left = currentPosition - 5 + 'px';
+    //collision
+  }else if(currentPosition === ballPositionX || ballPositionY){
+    points++
+    console.log("point scored")
   }
 }
 
@@ -125,11 +159,16 @@ switch(e.keyCode) {
     goal.setAttribute ("class", "show");
     animateDiv();
   break;
-  case 37: 
-  moveLeft();
+  case 37:
+    if(game.classList.contains('field') == true){
+      moveLeft();
+    }
+ 
   break;
   case 39: 
-  moveRight();
+    if(game.classList.contains('field') == true){
+    moveRight();
+  }
   break;
 }
 
