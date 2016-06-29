2c5a5717a3c0a057b0f9abb0cbf2067b50eae2eb
Acacia Mack <acacialmack@gmail.com>
2016-06-29 07:09:24 -0400

    comments

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 
 window.onload = function () {
-  console.log("PAGE LOADED")
+ console.log("spacebar = start and shoot | left arrow = goalie left | right arrow = goalie right")
 
   var animation = new TimelineMax({ repeat: -1})
    
@@ -21,7 +21,7 @@ window.addEventListener("keydown", function(e) {
 //if score equals 0 player ties
 //if score equals -number player loses
 //only 3 shots
-console.log("EVENT LISTENER")
+
 
 
 var player           = document.getElementById('player');
@@ -40,18 +40,6 @@ var ballPositionX = randX;
 var ballPositionY = randY;
 
 
-// console.log(start)
-// console.log(end)
-// var ballPosition  = parseInt(getComputedStyle(ball).left);
-// var shots            = 0;
-// console.log(field.offsetWidth)
-// console.log(player.offsetLeft)
-
-// console.log(player.offsetWidth);
-
-
-// parseInt(getComputedStyle(player).left)
-
 
 //add player, ball and goal once home becomes field
 switch(e.keyCode) {
@@ -62,8 +50,8 @@ switch(e.keyCode) {
     ball.setAttribute ("class", "show");
     goal.setAttribute ("class", "show");
     if(game.classList.contains('field') == true){
-      threeShots();
-      winner();
+      startGame();
+      
     };
   break;
   case 37:
@@ -80,106 +68,93 @@ switch(e.keyCode) {
 }
 
 
-//player can move right and left
+//player can move right
 function moveRight(){
 if(currentPosition < end) {
   paddleCollision()
-  console.log("move right");
-  console.log(currentPosition + "= currentPosition");
     player.style.left = currentPosition + 5 + 'px';
   }
 }
 
+//player can move left
 function moveLeft(){
 if(currentPosition > start) {
   paddleCollision()
-  console.log("move left");
-  console.log(currentPosition + "= currentPosition");
     player.style.left = currentPosition - 5 + 'px';
   }
 }
 
 //ball moves toward goal 3 times 
-function animateDiv(){
+function startGame(){
     var x = field.offsetWidth - 150;
     var y = field.offsetHeight - 150;
-    // console.log("x = " + x);
-    // console.log("y = " + y);
-
 
     var randX = Math.floor(Math.random(x) * x);
     var randY = Math.floor(Math.random(y) * y);
 
-    console.log(randX)
-    console.log(randY)
-
     var ballPositionX = randX;
-    var ballPositionY = randY;
-    console.log(currentPosition);  
+    var ballPositionY = randY; 
     var id = setInterval(frame, 10);
-   console.log(id)
+
     function frame() {
+
       if (ballPositionX == field.offsetWidth-goal.offsetHeight || ballPositionY == field.offsetHeight-goal.offsetHeight) {
         clearInterval(id);
       } else if (currentPosition === ballPositionX || ballPositionY) {
         ballPositionX ++;
         ballPositionY ++;
-        // console.log("Y = " + ballPositionY);
-        // console.log("X = " + ballPositionX);
         ball.style.top =  ballPositionY + 'px';
         ball.style.left = ballPositionX + 'px';
       }
+      paddleCollision();
+      threeShots();
     }
   } 
 
 
   function threeShots() {
     var shots = 0
-  
-   
+  for (shots=1; shots <= 3; shots++){
     if(shots <= 3){
-      shots ++;
-      console.log(shots)  
-      animateDiv()
-      }else{
+      // shots ++;
+      console.log("shot # " + shots) 
+    }else if (shots = 3){
+      winner()
         console.log("game over")
-      }
+    }
   }
+}
 
-  function winner() {
-      var points = 0;
-    if(points == 1||2||3 && shots === 3) {
+  var points = 0;
+
+  function winner() { 
+    var points
+
+    if(points === 1||2||3 && shots === 3) {
       console.log(points)
       console.log("winner, winner chicken dinner! Game Over")
-    } else if (points == 0 && shots === 3){
+    } else if (points === 0 && shots === 3){
       console.log("looks like a tie. Game Over")
-    } else if (points == -1 || -2 || -3 && shots === 3) {
+    } else if (points === -1 || -2 || -3 && shots === 3) {
       console.log("you need a bit more practice, mate. Game Over")
     }
   }
 
 // paddle collision
 function paddleCollision(){
-  var points = 0;
   if(currentPosition === ballPositionX || ballPositionY){
     points += 1;
     console.log("point scored")
-    // console.log(currentPosition + " = paddlecurrentPosition paddleCollision");
-    // console.log(ballPositionX + " =ballPositionX paddleCollision");
-    // console.log(ballPositionY + " =ballPositionY paddleCollision");
     console.log("points = " + points)
     //75 = ballPositionY Max
     return points
-  }else if(currentPosition > ballPositionX){
+  }else if(currentPosition > ballPositionX || ballPositionY){
     points -= 1;
     console.log("point loss")
-    // console.log(currentPosition + " = paddlecurrentPosition paddleCollision");
-    // console.log(ballPositionX + " =ballPositionX paddleCollision");
-    // console.log(ballPositionY + " =ballPositionY paddleCollision");
     console.log("points = " + points)
     return points
   }else {
-    console.log("game over!!!!")
+    console.log("0 points scored")
   }
   }
 })
